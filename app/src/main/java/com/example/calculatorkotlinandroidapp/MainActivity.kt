fd5d4672e66604afa24b5a7e303332739a1c36e6
Joao Favero <jvfavero@gmail.com>
2022-10-07 20:26:41 -0300

    add AUTHOR

--- app/src/main/java/com/example/calculatorkotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorkotlinandroidapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.calculatorkotlinandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun numberActon(view: View) {}
+    fun operationAction(view: View) {}
+    fun allClearAction(view: View) {
+
+    }
+    fun backSpaceAction(view: View) {
+    }
+    fun equalsAction(view: View) {}
 }
